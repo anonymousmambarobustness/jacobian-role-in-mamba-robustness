b36ca23c9936ea9c4876cf7b8d57fa7741c48aed
anonymousmambarobustness <anonymousmambarobustness@gmail.com>
2025-06-10 20:22:27 +0300

    Update cifar10.py

--- SSM_robustness/utils/cifar10.py
+++ SSM_robustness/utils/cifar10.py
@@ -1,6 +1,6 @@
 import os
 import re
-from models.SSM import SSM, Mega, S5_SSM, S6_SSM
+from models.SSM import SSM, S5_SSM, S6_SSM
 def build_model(args, model_name):
     original_model_name = model_name
     patch_size = int(model_name.split('_')[-1]) if "patch_size" in model_name else None
@@ -24,8 +24,6 @@ def build_model(args, model_name):
             model = SSM(d_input=3, n_layers=args.num_layers, mode = 'diag', patch_size=patch_size)
     elif model_name == 'S5':
         model = S5_SSM(d_input=3, n_layers=args.num_layers, patch_size=patch_size)
-    elif model_name == 'Mega':
-        model = Mega(d_input=3, n_layers=args.num_layers, patch_size=patch_size)
     elif model_name == 'S6':
         model = S6_SSM(d_input=3, n_layers=num_layers, reg_type=args.reg_type, seq_length=1024,use_AdSS=args.use_AdSS,AdSS_Type=args.AdSS_Type,patch_size=patch_size)
     return model
